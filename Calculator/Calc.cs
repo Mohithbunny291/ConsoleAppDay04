cba32f1daf39b7a1eb6634eb126390f562dc18a9
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-16 09:49:42 +0530

    Calculation Added

---
+++ Calculator/Calc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay04
+{
+    public class Calc
+    {
+        public void Add(double n1,double n2)
+        {
+            Console.WriteLine("Add " + n1 + n2);
+        }
+        public void Sub(double n1, double n2)
+        {
+            Console.WriteLine("Sub " +(n1 - n2));
+        }
+        public void Mul(double n1, double n2)
+        {
+            Console.WriteLine("Mul " + n1 * n2);
+        }
+        public void Div(double n1, double n2)
+        {
+            Console.WriteLine("Div " + n1 / n2);
+        }
+    }
+}
