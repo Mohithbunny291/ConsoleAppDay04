01e2f4c2367b528819a8c3a8597e7134abe3bc11
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-16 09:31:48 +0530

    Add project files.

---
+++ ConsoleAppDay04/NumberSystem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDay04
+{
+    public class NumberSystem
+    {
+        static int[] nums = new int[] { 12, 12, 4, 3, 12, 43, 12, 45, 5, 6, 773, 565 };
+        public static void OddNum()
+        {
+            foreach (int i in nums)
+            {
+                if(i % 2 == 1) 
+                    Console.WriteLine(i);
+            }
+        }
+        public static void EvenNum()
+        {
+            foreach (int i in nums)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ ConsoleAppDay04/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAppDay04;
+
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Odd Numbers are:");
+NumberSystem.OddNum();
+
+Console.WriteLine("Even Numbers are:");
+NumberSystem.EvenNum();
