efa67126d24d85a5f40e2e3b2d4348b01db047f1
Mohith Reddy <Mohithbunny291@gmail.com>
2024-02-16 09:55:58 +0530

    Addtion error rectified

--- Calculator/Calc.cs
+++ Calculator/Calc.cs
@@ -10,7 +10,7 @@ namespace ConsoleAppDay04
     {
         public void Add(double n1,double n2)
         {
-            Console.WriteLine("Add " + n1 + n2);
+            Console.WriteLine("Add " + (n1 + n2));
         }
         public void Sub(double n1, double n2)
         {
